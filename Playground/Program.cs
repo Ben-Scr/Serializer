400ed903759b52f98a1bfe5d9fcb358349b7110c
Ben <benscr.email@gmail.com>
2025-10-27 17:05:29 +0100

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,4 +1,6 @@
-﻿using BenScr.Serialization.Json;
+﻿using BenScr.Serialization.Binary;
+using BenScr.Serialization.Json;
+using BenScr.Serialization.Xml;
 public static class Program
 {
     public static void Main(string[] args)
@@ -6,6 +8,10 @@ public static class Program
         string path = "Test.json";
         Json.Save(path, 5);
         int loadedInt = Json.Load<int>(path);
+        Console.WriteLine(loadedInt);
+
+        Xml.Save(path, 5);
+        loadedInt = Xml.Load<int>(path);
         Console.WriteLine(loadedInt);
     }
 }

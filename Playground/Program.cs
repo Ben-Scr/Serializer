e43ae53506a652bb4e66b765fb96f4a623c3b8ca
Ben <benscr.email@gmail.com>
2025-11-23 10:17:34 +0100

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -19,6 +19,21 @@ public static class Program
 
     public static void Main(string[] args)
     {
+        const string pathJson = "Preview.json", pathXml = "Preview.xml", pathBin = "Preview.bin";
+        Vector2 vec2 = new Vector2 { X = 1.0f, Y = 2.0f };
 
+        Console.WriteLine("Object to save: " + vec2);
+
+        Json.Save(pathJson, vec2);
+        Vector2 loadedVec2 = Json.Load<Vector2>(pathJson);
+        Console.WriteLine("Json: " + loadedVec2);
+        
+        Xml.SaveCompressed(pathXml, vec2);
+        loadedVec2 = Xml.LoadCompressed<Vector2>(pathXml);
+        Console.WriteLine("Xml: " + loadedVec2);
+
+        Binary.Save(pathBin, vec2);
+        loadedVec2 = Binary.Load<Vector2>(pathBin);
+        Console.WriteLine("Binary: " + loadedVec2);
     }
 }
